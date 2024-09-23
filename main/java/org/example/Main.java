c05e025463499de05a1664e5f9aace387c085f6d
brezginaor <obrezgina@mail.ru>
2024-09-23 16:23:11 +0300

    modified

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Server server=new Server(8082);
-        server.startServer();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
